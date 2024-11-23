00a0e1abb8cb83f699a7b0063a9a7a29654e4098
nqthahh <thanhuzu123@gmail.com>
2024-11-24 03:14:51 +0700

    Delete BAITAP1 directory

--- BAITAP1/bai1/bai1/main.js
+++
@@ -1,6 +0,0 @@
-function chuyenDoi() {
-    const doC = document.getElementById('inputC').value; //Lấy giá trị nhập từ ô có id là "inputC" trong html
-    const doF = (doC * 9 / 5) + 32;
-    document.getElementById('outputF').value = doF;
-
-}
